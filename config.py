5892b500a9d82c33c1d9a861344ffe94f962d42d
venuk-1502 <69380804+venuk-1502@users.noreply.github.com>
2022-06-28 22:39:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    #APP_ID = os.environ.get("MicrosoftAppId", "")
+    #APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = "9b7413bd-727e-4fb8-850a-4a740c173d37"
+    APP_PASSWORD = "aq.p~6seY7.~iTbNv909IsFa34y8gOS3rz"
